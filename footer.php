d0ed8f0ef56419130480ab8c067baffeaeabf1fc
Jeffry Tupa <jeffry@tupadesigns.com>
2016-05-10 22:41:34 -0500

    wordpress twentysixteen theme with polymer

---
+++ footer.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Polytize Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'polytizetwentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'primary',
+							'menu_class'     => 'primary-menu',
+						 ) );
+					?>
+				</nav><!-- .main-navigation -->
+			<?php endif; ?>
+
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'polytizetwentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'social',
+							'menu_class'     => 'social-links-menu',
+							'depth'          => 1,
+							'link_before'    => '<span class="screen-reader-text">',
+							'link_after'     => '</span>',
+						) );
+					?>
+				</nav><!-- .social-navigation -->
+			<?php endif; ?>
+
+			<div class="site-info">
+				<?php
+					/**
+					 * Fires before the polytizetwentysixteen footer text for footer customization.
+					 *
+					 * @since Polytize Twenty Sixteen 1.0
+					 */
+					do_action( 'twentysixteen_credits' );
+				?>
+				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'polytizetwentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'polytizetwentysixteen' ), 'WordPress' ); ?></a>
+			</div><!-- .site-info -->
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
